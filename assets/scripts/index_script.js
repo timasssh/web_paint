8631fc1c2af51a008aafade515bd24e155b2c7a9
timas <timmylbueno@gmail.com>
2025-11-14 15:08:15 -0300

    implementation of the 'eraser tool' and 'undo' funcionalities, that use the 'removeStroke' function to, respectively, erase strokes and erase the last stroke made

--- assets/scripts/index_script.js
+++ assets/scripts/index_script.js
@@ -1,9 +1,10 @@
 const formTools = document.getElementById("controls");
-let paint_board = document.getElementById("paint_board");
+const paint_board = document.getElementById("paint_board");
 const undoButton = document.getElementById("undo_button");
 const saveImageButton = document.getElementById("save-image");
 
 let counter = 0;
+let strokeClassIdentifier = 0;
 
 let initialFormData = new FormData(formTools);
 let selectedColor = initialFormData.get("color");
@@ -18,27 +19,43 @@ formTools.addEventListener("input", (event) => {
 
     selectedColor = formData.get("color");
     selectedBrushSize = formData.get("paint-brush-size");
+    let selectedTool = formData.get("selectedTool");
 
-    if(formData.get("selectedTool") === "paint-brush") {
+    if(selectedTool === "paint-brush") {
         paint_board.addEventListener("mousedown", paint_brush);
+    }else if(selectedTool === "eraser") {
+        paint_board.addEventListener("mousedown", eraser);
     }
-})
-undoButton.addEventListener("click", () => {
-    console.log("undoing the last stroke...");
-})
+});
 
 
 function removeEventListersTool() {
     paint_board.removeEventListener("mousedown", paint_brush);
+    paint_board.removeEventListener("mousedown", eraser);
+}
+function increaseCounter() {
+    counter++;
+    strokeClassIdentifier++;
+
+    console.log(counter + " " + strokeClassIdentifier);
+    
+
+    undoButton.classList.remove("disabledIcon");
+    undoButton.addEventListener("click", undo);
+}
+function decreaseCounter() {
+    counter--;
+    undoButton.classList.add("disabledIcon");
+    undoButton.removeEventListener("click", undo);
 }
 
 function createPixel() {
-    if(event.target.id !== paint_board.id) { return false}
+    if(event.target.id !== paint_board.id) { return false }
     
     let span = document.createElement("span");
     span.style.backgroundColor = selectedColor;
     span.classList.add("stroke");
-    span.classList.add(`stroke${counter}`);
+    span.classList.add(`stroke${strokeClassIdentifier}`);
     span.style.left = `${event.layerX}px`;
     span.style.top = `${event.layerY}px`;
     span.style.height = `${selectedBrushSize}px`;
@@ -49,13 +66,12 @@ function createPixel() {
     return true;
 }
 function paint_brush() {
-    if(!createPixel()) {return false};
+    if(!createPixel()) { return };
     paint_board.addEventListener("mousemove", createPixel);
 
     function stopPainting() {
         paint_board.removeEventListener("mousemove", createPixel);
-        counter++;
-        console.log(counter);
+        increaseCounter();
 
         paint_board.removeEventListener("mouseup", stopPainting);
         paint_board.removeEventListener("mouseleave", stopPainting);
@@ -65,6 +81,40 @@ function paint_brush() {
     paint_board.addEventListener("mouseleave", stopPainting);
 }
 
+function removeStroke() {
+    let strokeClass = typeof arguments[0] === "string" ? arguments[0] : event.target.classList[1];
+    
+    let strokeSpans = paint_board.querySelectorAll("span." + strokeClass);
+
+    strokeSpans.forEach((span) => {
+        span.remove();
+    });
+
+    return true;
+}
+function eraser() {
+    paint_board.addEventListener("mousemove", removeStroke);
+
+    function stopErasing() {
+        paint_board.removeEventListener("mousemove", removeStroke);
+
+        paint_board.removeEventListener("mouseup", stopErasing);
+        paint_board.removeEventListener("mouseleave", stopErasing);
+
+        console.log("stoping erase");
+    }
+
+    paint_board.addEventListener("mouseleave", stopErasing);
+    paint_board.addEventListener("mouseup", stopErasing);
+}
+
+function undo() {
+    decreaseCounter();
+    let lastStrokeClass = `stroke${strokeClassIdentifier - 1}`;
+
+    removeStroke(lastStrokeClass);
+}
+undoButton.addEventListener("click", undo);
 saveImageButton.addEventListener("click", () => {
     domtoimage.toPng(paint_board)
         .then((dataUrl) => {
